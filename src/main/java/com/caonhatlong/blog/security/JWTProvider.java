0bc4877546f87357f793167bb1fde28d2217b592
harusora <longcnhe141293@gmail.com>
2022-12-25 23:52:13 +0700

    jwt using public and private key

--- src/main/java/com/caonhatlong/blog/security/JWTProvider.java
+++ src/main/java/com/caonhatlong/blog/security/JWTProvider.java
@@ -9,33 +9,58 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
 public class JWTProvider {
 
-    private Key key;
+    private KeyStore keyStore;
     @PostConstruct
     public void init(){
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream inputStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(inputStream, "longvipchtq1".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String generateTokent(Authentication authentication){
         User principal = (User) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(principal.getUsername())
-                .signWith(key)
+                .signWith(getPrivateKey())
                 .compact();
     }
 
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey)keyStore.getKey("springblog", "longvipchtq1".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException |UnrecoverableKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public boolean validateToken(String jwt){
-        Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
         return true;
+    }
+
+    private PublicKey getPublicKey(){
+        try {
+            return keyStore.getCertificate("springblog").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String getUserNameFromJWT(String token) {
         Claims claims = Jwts.parser()
-                .setSigningKey(key)
+                .setSigningKey(getPublicKey())
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
